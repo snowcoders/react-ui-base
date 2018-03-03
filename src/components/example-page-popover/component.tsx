980ae086769c08e748b1208a41c79f7fce9898e1
Ricky Patel <rickyp@snowcoders.com>
2018-03-02 22:07:11 -0800

    Got it working, just not building

--- src/components/example-page-popover/component.tsx
+++ src/components/example-page-popover/component.tsx
@@ -3,24 +3,19 @@ import * as React from 'react';
 import { Manager, Target, Popper, Arrow } from '@snowcoders/react-popper';
 
 import "@snowcoders/react-popover/styles.css";
-import { TargetHover, TargetClick, PopperBlur, PopperClick, PopperHover } from '@snowcoders/react-popover';
+import { Popover, PopperType, TargetType } from '@snowcoders/react-popover';
 
 import { ExampleComponentBase } from "../example-component-base";
 import { ExamplePageBase } from '../example-page-base';
 
 import * as classnames from "classnames";
 
-export type AvailableClassNames = "hover" | "hard-dismiss" | "soft-dismiss";
-
 export interface IExamplePagePopoverProps {
 }
 
 export interface IExamplePagePopoverState {
-    classNameToApply: AvailableClassNames;
-    isHoveringPopover: boolean;
-    isHoveringTarget: boolean;
-    isHardDismissOpen: boolean;
-    isSoftDismissOpen: boolean;
+    targetType: TargetType,
+    popperType: PopperType,
 }
 
 export class ExamplePagePopover extends React.Component<IExamplePagePopoverProps, IExamplePagePopoverState> {
@@ -28,153 +23,99 @@ export class ExamplePagePopover extends React.Component<IExamplePagePopoverProps
         super(props);
 
         this.state = {
-            classNameToApply: "hover",
-            isHoveringPopover: false,
-            isHoveringTarget: false,
-            isHardDismissOpen: false,
-            isSoftDismissOpen: false
+            targetType: "hover",
+            popperType: "hover"
         };
     }
 
     render() {
-        let classNameToName: { className: AvailableClassNames, name: string }[] = [{
-            className: "hover",
-            name: "Hover example"
-        },
-        {
-            className: "hard-dismiss",
-            name: "Hard dismiss example"
-        },
-        {
-            className: "soft-dismiss",
-            name: "Soft dismiss example"
-        }];
+        let popperTypeToName: { popperType: PopperType, name: string }[] = [
+            {
+                popperType: "blur",
+                name: "Blur"
+            },
+            {
+                popperType: "click",
+                name: "Click"
+            }, {
+                popperType: "hover",
+                name: "Hover"
+            }];
+        let targetTypeToName: { targetType: TargetType, name: string }[] = [
+            {
+                targetType: "click",
+                name: "Click"
+            }, {
+                targetType: "hover",
+                name: "Hover"
+            }];
 
         return <ExamplePageBase
             componentName="Popover"
             npmPackageName="@snowcoders/react-popover"
             componentDescription="The popover some element that expands more content from a parent element. Tooltips, dropdowns, selects, menus and more fit into this category. Instead of writing code for each of these individually, we instead wrote a few base helper classes which should allow a developer to customize them into any specific subtype."
+            faq={[<span>If your arrow is showing up in the upper left hand corner, please make sure you are installing popper.js version <code>1.13.0-next.1</code> or higher. We require the <code>positionFixed</code> property to be available.</span>]}
             githubUrl="https://github.com/snowcoders/react-popover"
             npmUrl="https://www.npmjs.com/package/@snowcoders/react-popover"
             exampleSrcUrl="https://github.com/snowcoders/react-ui-base/tree/master/src/components/example-page-popover"
             examples={[{
                 description: "A few different popover examples",
-                source: "import \"@snowcoders/react-popover/styles.css\";\nimport { Manager, Arrow } from '@snowcoders/react-popper';\nimport { TargetHover, TargetClick, PopperBlur, PopperClick, PopperHover } from '@snowcoders/react-popover';\n\n",
                 example:
                     <div className="sci-react-ui-base-example-page-popover">
                         <div className="styleSelector">
+                            Target Type
+                        {
+                                targetTypeToName.map((value) => {
+                                    return <label key={value.targetType}><input type="radio" name="target" onChange={this.getOnTargetTypeClick(value.targetType)} defaultChecked={this.state.targetType === value.targetType} />{value.name}</label>
+                                })
+                            }
+                        </div>
+                        <div className="styleSelector">
+                            Popper Type
                             {
-                                classNameToName.map((value) => {
-                                    return <label key={value.className}><input type="radio" name="style" onChange={this.getOnClick(value.className)} defaultChecked={this.state.classNameToApply === value.className} />{value.name}</label>
+                                popperTypeToName.map((value) => {
+                                    return <label key={value.popperType}><input type="radio" name="popper" onChange={this.getOnPopperTypeClick(value.popperType)} defaultChecked={this.state.popperType === value.popperType} />{value.name}</label>
                                 })
                             }
                         </div>
                         <div className="popover-example">
-                            <Manager>
-                                <div className={classnames("container", "mouse-over", { visible: this.state.classNameToApply === "hover" })}>
-                                    <div className="example">
-                                        <span>The hover example uses a</span>
-                                        {" "}
-                                        <TargetHover
-                                            className="target"
-                                            onHoverChange={(isHovering: boolean) => {
-                                                this.setState({ isHoveringTarget: isHovering })
-                                            }}>
-                                            TargetHover
-                                    </TargetHover>
-                                        <PopperHover
-                                            positionFixed={true}
-                                            placement="right"
-                                            className={classnames("popper", { visible: this.state.isHoveringTarget || this.state.isHoveringPopover })}
-                                            onHoverChange={(isHovering: boolean) => { this.setState({ isHoveringPopover: isHovering }) }}
-                                        >
-                                            I'm <button onClick={() => { alert("hi"); }}>the</button> popover
-                                    </PopperHover>
-                                        {" "}
-                                        <span>along with a PopoverHover.</span>
-                                    </div>
-                                    <p>The popover will dismiss when the mouse is hovering on neither the Target Box or the popover</p>
-                                    <p>
-                                        Examples of this style:
-                                        <ul>
-                                            <li><a href="https://material-ui-next.com/demos/tooltips/" target="_blank">Material UI's Tooltip</a></li>
-                                        </ul>
-                                    </p>
-                                </div>
-                            </Manager>
-                            <Manager>
-                                <div className={classnames("container", "hard-dismiss", { visible: this.state.classNameToApply === "hard-dismiss" })}>
-                                    <div className="example">
-                                        <span>The hard dismiss uses a</span>
-                                        {" "}
-                                        <TargetClick
-                                            className="target"
-                                            onClick={() => {
-                                                this.setState({ isHardDismissOpen: true })
-                                            }}>
-                                            TargetClick
-                                    </TargetClick>
-                                        <PopperClick
-                                            positionFixed={true}
-                                            placement="right"
-                                            className={classnames("popper", { visible: this.state.isHardDismissOpen })}
-                                            onDismiss={() => { this.setState({ isHardDismissOpen: false }) }}
-                                        >
-                                            I'm <button onClick={() => { alert("hi"); }}>the</button> popover
-                                    </PopperClick>
-                                        {" "}
-                                        <span> along with a PopoverClick.</span>
-                                    </div>
-                                    <p>The PopoverClick requires the user to explicitly click on the background to dismiss the popover.</p>
-                                    <p>
-                                        Examples of this style:
-                                        <ul>
-                                            <li><a href="https://material-ui-next.com/demos/menus/" target="_blank">Material UI's Menus</a></li>
-                                        </ul>
-                                    </p>
-                                </div>
-                            </Manager>
-                            <Manager>
-                                <div className={classnames("container", "soft-dismiss", { visible: this.state.classNameToApply === "soft-dismiss" })}>
-                                    <div className="example">
-                                        <span>The soft dismiss uses a</span>
-                                        {" "}
-                                        <TargetClick
-                                            className="target"
-                                            onClick={() => {
-                                                this.setState({ isSoftDismissOpen: true })
-                                            }}>
-                                            TargetClick
-                                    </TargetClick>
-                                        <PopperBlur
-                                            positionFixed={true}
-                                            placement="right"
-                                            className={classnames("popper", { visible: this.state.isSoftDismissOpen })}
-                                            onDismiss={() => { this.setState({ isSoftDismissOpen: false }) }}
-                                        >
-                                            I'm <button onClick={() => { alert("hi"); }}>the</button> popover
-                                    </PopperBlur>
-                                        {" "}
-                                        <span> along with a PopoverBlur.</span>
-                                    </div>
-                                    <p>The PopoverBlur requires the user to click on the background to dismiss the popover but unlike the hard dismiss, the click will be propagated.</p>
-                                    <p>
-                                        Examples of this style:
-                                        <ul>
-                                            <li><a href="https://jqueryui.com/selectmenu/" target="_blank">jQuery UI's Select menu</a></li>
-                                        </ul>
-                                    </p>
-                                </div>
-                            </Manager>
+                            <div className="example">
+                                <Popover
+                                    popperContent={"Popper type - " + this.state.popperType}
+                                    popperOptions={{
+                                        positionFixed: true,
+                                        placement: "right"
+                                    }}
+                                    popperType={this.state.popperType}
+                                    targetContent={"Target type - " + this.state.targetType}
+                                    targetType={this.state.targetType}
+                                />
+                            </div>
+                            <p>
+                                Real world examples:
+                                <ul>
+                                    <li>Hover + Hover - <a href="https://material-ui-next.com/demos/tooltips/" target="_blank">Material UI's Tooltip</a></li>
+                                    <li>Click + Click - <a href="https://material-ui-next.com/demos/menus/" target="_blank">Material UI's Menus</a></li>
+                                    <li>Click + Blur - <a href="https://jqueryui.com/selectmenu/" target="_blank">jQuery UI's Select menu</a></li>
+                                </ul>
+                            </p>
                         </div>
                     </div>
             }]} />;
     }
 
-    getOnClick(className: AvailableClassNames) {
+    getOnTargetTypeClick(targetType: TargetType) {
         return () => {
             this.setState({
-                classNameToApply: className
+                targetType: targetType
+            });
+        };
+    };
+
+    getOnPopperTypeClick(popperType: PopperType) {
+        return () => {
+            this.setState({
+                popperType: popperType
             });
         };
     };
