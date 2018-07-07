cb366cf71e8e8708a2da6d94ff2f7cde74730468
Ricky Patel <rickyp@snowcoders.com>
2018-07-06 17:35:12 -0700

    Added clearer explanation of how to override styles

--- src/components/usage/component.tsx
+++ src/components/usage/component.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
+import { CodeHighlighter } from '../code-highlighter';
 
 export class Usage extends React.Component {
     render() {
         return (
             <div className="sci-react-ui-base-usage">
-                <h1>Usage and versioning</h1>
+                <h1>Usage</h1>
+                <p>When overriding our base styles, we recommend overriding based on specificity. Taking our react-unstyled-button component as an example, the following is the suggested way to override styles</p>
+                <CodeHighlighter javascriptSourceCode={`.sci-react-unstyled-button.my-custom-button-class {
+    margin: 50px;
+}`}></CodeHighlighter>
+                <p>Remember that these components are not meant to be a UI library themselves but the core basics to allow you to create your own</p>
+                <h1>Versioning</h1>
                 <p>We use <a href="https://semver.org/">Semver</a> in a very specific way to help manage upgrades while not completely breaking your system as we fix bugs. For our packages, please read the following:</p>
                 <ol>
                     <li>Major version - Javascript nor CSS is backwards compatible... you'll need to rewrite the component</li>
