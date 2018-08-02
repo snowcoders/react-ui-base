8e5329d617631ede8390f8655622c2ce9b851098
Ricky Patel <rickyp@snowcoders.com>
2018-08-02 02:00:47 -0700

    Ran sortier on code base

--- src/components/example-page-dialog/component.tsx
+++ src/components/example-page-dialog/component.tsx
@@ -1,105 +1,142 @@
-import * as React from 'react';
+import * as React from "react";
 
 import { Dialog } from "@snowcoders/react-dialog";
 import "@snowcoders/react-dialog/styles.css";
 
 import { ExampleComponentBase } from "../example-component-base";
-import { ExamplePageBase } from '../example-page-base';
+import { ExamplePageBase } from "../example-page-base";
 
 import * as classnames from "classnames";
 
 export type AvailableClassNames = "default" | "google" | "jquery-ui" | "office";
 
-export interface IExamplePageDialogProps {
-}
+export interface IExamplePageDialogProps {}
 
 export interface IExamplePageDialogState {
-    classNameToApply: AvailableClassNames;
-    isDialogOpen: boolean;
+  classNameToApply: AvailableClassNames;
+  isDialogOpen: boolean;
 }
 
-export class ExamplePageDialog extends React.Component<IExamplePageDialogProps, IExamplePageDialogState> {
-    constructor(props: IExamplePageDialogProps) {
-        super(props);
+export class ExamplePageDialog extends React.Component<
+  IExamplePageDialogProps,
+  IExamplePageDialogState
+> {
+  constructor(props: IExamplePageDialogProps) {
+    super(props);
 
-        this.state = {
-            classNameToApply: "google",
-            isDialogOpen: false,
-        };
-    }
-
-    render() {
-
-        let classNameToName: { className: AvailableClassNames, name: string }[] = [{
-            className: "google",
-            name: "Google's Material UI"
-        }, {
-            className: "office",
-            name: "Microsoft Office's Fabric UI"
-        }];
-
-        return <ExamplePageBase
-            componentDescription="The dialog is a basic positioning component that puts the content centered on the screen over the rest of the page"
-            componentName="Dialog"
-            examples={[{
-                description: "Dialog component with styling of a few UI libraries",
-                example: (
-                    <div className="sci-react-ui-base-example-page-dialog">
-                        <div className="styleSelector">
-                            {
-                                classNameToName.map((value) => {
-                                    return <label key={value.className}><input type="radio" name="style" onChange={this.getOnClick(value.className)} defaultChecked={this.state.classNameToApply === value.className} />{value.name}</label>
-                                })
-                            }
-                        </div>
-                        <div>
-                            <button onClick={this.getOnOpenDialogToggle(true)}>
-                                Open dialog
-                            </button>
-                        </div>
-                        <Dialog
-                            className={classnames(this.state.classNameToApply)}
-                            isVisible={this.state.isDialogOpen}
-                            onBackgroundClick={this.getOnOpenDialogToggle(false)}
-                        >
-                            <h3>Dialog</h3>
-                            <input className="a" type="text" />
-                            <input className="b" type="text" />
-                            <div className="content">
-                                <span>Often times there are cancel, submit and close buttons inside the dialog. These vary from library to library so we left the contents of this div to be whatever you may wish. That said, you do get a few things for free:</span>
-                                <ul>
-                                    <li>Clicking the grey overlay fires a callback, which you can either close or not close the dialog</li>
-                                    <li>Pressing escape will fire the same callback as the overlay</li>
-                                    <li>Tabbing in the dialog will not leave this dialog</li>
-                                </ul>
-                                <span>If you need to support accessibility scenarios, here are some implementation suggestions</span>
-                                <ul>
-                                    <li>Always have some sort of button to close the dialog. Without this a screen reader will not announce how to close the dialog</li>
-                                </ul>
-                            </div>
-                        </Dialog>
-                    </div>)
-            }]}
-            exampleSrcUrl="https://github.com/snowcoders/react-ui-base/tree/master/src/components/example-page-dialog"
-            githubUrl="https://github.com/snowcoders/react-dialog"
-            npmPackageName="@snowcoders/react-dialog"
-            npmUrl="https://www.npmjs.com/package/@snowcoders/react-dialog"
-        />;
-    }
-
-    getOnOpenDialogToggle = (isDialogOpen: boolean) => {
-        return () => {
-            this.setState({
-                isDialogOpen: isDialogOpen
-            });
-        }
+    this.state = {
+      classNameToApply: "google",
+      isDialogOpen: false
     };
+  }
 
-    getOnClick(className: AvailableClassNames) {
-        return () => {
-            this.setState({
-                classNameToApply: className
-            });
-        };
+  render() {
+    let classNameToName: { className: AvailableClassNames; name: string }[] = [
+      {
+        className: "google",
+        name: "Google's Material UI"
+      },
+      {
+        className: "office",
+        name: "Microsoft Office's Fabric UI"
+      }
+    ];
+
+    return (
+      <ExamplePageBase
+        componentDescription="The dialog is a basic positioning component that puts the content centered on the screen over the rest of the page"
+        componentName="Dialog"
+        examples={[
+          {
+            description: "Dialog component with styling of a few UI libraries",
+            example: (
+              <div className="sci-react-ui-base-example-page-dialog">
+                <div className="styleSelector">
+                  {classNameToName.map(value => {
+                    return (
+                      <label key={value.className}>
+                        <input
+                          type="radio"
+                          name="style"
+                          onChange={this.getOnClick(value.className)}
+                          defaultChecked={
+                            this.state.classNameToApply === value.className
+                          }
+                        />
+                        {value.name}
+                      </label>
+                    );
+                  })}
+                </div>
+                <div>
+                  <button onClick={this.getOnOpenDialogToggle(true)}>
+                    Open dialog
+                  </button>
+                </div>
+                <Dialog
+                  className={classnames(this.state.classNameToApply)}
+                  isVisible={this.state.isDialogOpen}
+                  onBackgroundClick={this.getOnOpenDialogToggle(false)}
+                >
+                  <h3>Dialog</h3>
+                  <input className="a" type="text" />
+                  <input className="b" type="text" />
+                  <div className="content">
+                    <span>
+                      Often times there are cancel, submit and close buttons
+                      inside the dialog. These vary from library to library so
+                      we left the contents of this div to be whatever you may
+                      wish. That said, you do get a few things for free:
+                    </span>
+                    <ul>
+                      <li>
+                        Clicking the grey overlay fires a callback, which you
+                        can either close or not close the dialog
+                      </li>
+                      <li>
+                        Pressing escape will fire the same callback as the
+                        overlay
+                      </li>
+                      <li>Tabbing in the dialog will not leave this dialog</li>
+                    </ul>
+                    <span>
+                      If you need to support accessibility scenarios, here are
+                      some implementation suggestions
+                    </span>
+                    <ul>
+                      <li>
+                        Always have some sort of button to close the dialog.
+                        Without this a screen reader will not announce how to
+                        close the dialog
+                      </li>
+                    </ul>
+                  </div>
+                </Dialog>
+              </div>
+            )
+          }
+        ]}
+        exampleSrcUrl="https://github.com/snowcoders/react-ui-base/tree/master/src/components/example-page-dialog"
+        githubUrl="https://github.com/snowcoders/react-dialog"
+        npmPackageName="@snowcoders/react-dialog"
+        npmUrl="https://www.npmjs.com/package/@snowcoders/react-dialog"
+      />
+    );
+  }
+
+  getOnOpenDialogToggle = (isDialogOpen: boolean) => {
+    return () => {
+      this.setState({
+        isDialogOpen: isDialogOpen
+      });
     };
+  };
+
+  getOnClick(className: AvailableClassNames) {
+    return () => {
+      this.setState({
+        classNameToApply: className
+      });
+    };
+  }
 }
--- src/components/example-page-popover/component.tsx
+++ src/components/example-page-popover/component.tsx
@@ -1,87 +1,99 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { Popover, PopperType, TargetType } from '@snowcoders/react-popover';
+import { Popover, PopperType, TargetType } from "@snowcoders/react-popover";
 import "@snowcoders/react-popover/styles.css";
 
 import { ExampleComponentBase } from "../example-component-base";
-import { ExamplePageBase } from '../example-page-base';
+import { ExamplePageBase } from "../example-page-base";
 
 import * as classnames from "classnames";
 
 type PopperPositionOptions = "bottom" | "left" | "right" | "top";
 
-export interface IExamplePagePopoverProps {
-}
+export interface IExamplePagePopoverProps {}
 
 export interface IExamplePagePopoverState {
-    popperMoreCount: number,
-    popperType: PopperType,
-    position: PopperPositionOptions,
-    targetType: TargetType,
+  popperMoreCount: number;
+  popperType: PopperType;
+  position: PopperPositionOptions;
+  targetType: TargetType;
 }
 
-export class ExamplePagePopover extends React.Component<IExamplePagePopoverProps, IExamplePagePopoverState> {
-    constructor(props: IExamplePagePopoverProps) {
-        super(props);
+export class ExamplePagePopover extends React.Component<
+  IExamplePagePopoverProps,
+  IExamplePagePopoverState
+> {
+  constructor(props: IExamplePagePopoverProps) {
+    super(props);
 
-        this.state = {
-            popperMoreCount: 0,
-            popperType: "hover",
-            position: "bottom",
-            targetType: "hover",
-        };
-    }
+    this.state = {
+      popperMoreCount: 0,
+      popperType: "hover",
+      position: "bottom",
+      targetType: "hover"
+    };
+  }
 
-    render() {
-        let popperTypeToName: { popperType: PopperType, name: string }[] = [{
-            name: "Hover",
-            popperType: "hover"
-        },
-        {
-            name: "Blur",
-            popperType: "blur"
-        },
-        {
-            name: "Click",
-            popperType: "click"
-        }, {
-            name: "None",
-            popperType: "none"
-        }];
-        let targetTypeToName: { targetType: TargetType, name: string }[] = [{
-            name: "Hover",
-            targetType: "hover"
-        },
-        {
-            name: "Click",
-            targetType: "click"
-        }];
-        let positionToName: { position: PopperPositionOptions, name: string }[] = [{
-            name: "Bottom",
-            position: "bottom"
-        },
-        {
-            name: "Top",
-            position: "top"
-        },
-        {
-            name: "Left",
-            position: "left"
-        }, {
-            name: "Right",
-            position: "right"
-        }];
+  render() {
+    let popperTypeToName: { popperType: PopperType; name: string }[] = [
+      {
+        name: "Hover",
+        popperType: "hover"
+      },
+      {
+        name: "Blur",
+        popperType: "blur"
+      },
+      {
+        name: "Click",
+        popperType: "click"
+      },
+      {
+        name: "None",
+        popperType: "none"
+      }
+    ];
+    let targetTypeToName: { targetType: TargetType; name: string }[] = [
+      {
+        name: "Hover",
+        targetType: "hover"
+      },
+      {
+        name: "Click",
+        targetType: "click"
+      }
+    ];
+    let positionToName: { position: PopperPositionOptions; name: string }[] = [
+      {
+        name: "Bottom",
+        position: "bottom"
+      },
+      {
+        name: "Top",
+        position: "top"
+      },
+      {
+        name: "Left",
+        position: "left"
+      },
+      {
+        name: "Right",
+        position: "right"
+      }
+    ];
 
-        return <ExamplePageBase
-            componentDescription="The popover some element that expands more content from a parent element. Tooltips, dropdowns, selects, menus and more fit into this category. Instead of writing code for each of these individually, we instead wrote a few base helper classes which should allow a developer to customize them into any specific subtype."
-            componentName="Popover"
-            exampleSrcUrl="https://github.com/snowcoders/react-ui-base/tree/master/src/components/example-page-popover"
-            githubUrl="https://github.com/snowcoders/react-popover"
-            npmPackageName="@snowcoders/react-popover"
-            npmUrl="https://www.npmjs.com/package/@snowcoders/react-popover"
-            examples={[{
-                description: "Popover example showing off target and popper types",
-                source: `import "@snowcoders/react-popover/styles.css";
+    return (
+      <ExamplePageBase
+        componentDescription="The popover some element that expands more content from a parent element. Tooltips, dropdowns, selects, menus and more fit into this category. Instead of writing code for each of these individually, we instead wrote a few base helper classes which should allow a developer to customize them into any specific subtype."
+        componentName="Popover"
+        exampleSrcUrl="https://github.com/snowcoders/react-ui-base/tree/master/src/components/example-page-popover"
+        githubUrl="https://github.com/snowcoders/react-popover"
+        npmPackageName="@snowcoders/react-popover"
+        npmUrl="https://www.npmjs.com/package/@snowcoders/react-popover"
+        examples={[
+          {
+            description: "Popover example showing off target and popper types",
+            source: `import "@snowcoders/react-popover/styles.css";
 import { Popover, PopperType, TargetType } from '@snowcoders/react-popover';
 
 // Note that wrapperElementType and wrapperElementProps are only needed for React 15
@@ -96,130 +108,200 @@ import { Popover, PopperType, TargetType } from '@snowcoders/react-popover';
     targetContent={"Target text"}
     targetType={"hover"}
 />`,
-                example:
-                    <div className="sci-react-ui-base-example-page-popover">
-                        <div className="left-pane">
-                            <div className="options">
-                                <div className="style-selector">
-                                    Target Type
-                        {
-                                        targetTypeToName.map((value) => {
-                                            return <label key={value.targetType}><input type="radio" name="target" onChange={this.getOnTargetTypeClick(value.targetType)} defaultChecked={this.state.targetType === value.targetType} />{value.name}</label>
-                                        })
-                                    }
-                                </div>
-                                <div className="style-selector">
-                                    Popper Type
-                            {
-                                        popperTypeToName.map((value) => {
-                                            return <label key={value.popperType}><input type="radio" name="popper" onChange={this.getOnPopperTypeClick(value.popperType)} defaultChecked={this.state.popperType === value.popperType} />{value.name}</label>
-                                        })
-                                    }
-                                </div>
-                                <div className="style-selector">
-                                    Placement
-                            {
-                                        positionToName.map((value) => {
-                                            return <label key={value.position}><input type="radio" name="position" onChange={this.getOnPositionClick(value.position)} defaultChecked={this.state.position === value.position} />{value.name}</label>
-                                        })
-                                    }
-                                </div>
-                            </div>
-                            <div className="examples">
-                                <p>
-                                    Real world examples:
-                            </p>
-                                <ul>
-                                    <li>Hover + Hover - <a href="https://material-ui-next.com/demos/tooltips/" target="_blank">Material UI's Tooltip</a></li>
-                                    <li>Click + Click - <a href="https://material-ui-next.com/demos/menus/" target="_blank">Material UI's Menus</a></li>
-                                    <li>Click + Blur - <a href="https://jqueryui.com/selectmenu/" target="_blank">jQuery UI's Select menu</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="popover-example">
-                            <div className="example">
-                                <Popover
-                                    wrapperElementType={"span"}
-                                    wrapperElementProps={{ className: "react-15" }}
-                                    popperContent={<PopoverContent prefixText={this.state.popperType} />}
-                                    popperOptions={{
-                                        placement: this.state.position
-                                    }}
-                                    popperType={this.state.popperType}
-                                    targetContent={"Target type - " + this.state.targetType}
-                                    targetType={this.state.targetType}
-                                />
-                            </div>
-                        </div>
+            example: (
+              <div className="sci-react-ui-base-example-page-popover">
+                <div className="left-pane">
+                  <div className="options">
+                    <div className="style-selector">
+                      Target Type
+                      {targetTypeToName.map(value => {
+                        return (
+                          <label key={value.targetType}>
+                            <input
+                              type="radio"
+                              name="target"
+                              onChange={this.getOnTargetTypeClick(
+                                value.targetType
+                              )}
+                              defaultChecked={
+                                this.state.targetType === value.targetType
+                              }
+                            />
+                            {value.name}
+                          </label>
+                        );
+                      })}
                     </div>
-            }]} />;
-    }
+                    <div className="style-selector">
+                      Popper Type
+                      {popperTypeToName.map(value => {
+                        return (
+                          <label key={value.popperType}>
+                            <input
+                              type="radio"
+                              name="popper"
+                              onChange={this.getOnPopperTypeClick(
+                                value.popperType
+                              )}
+                              defaultChecked={
+                                this.state.popperType === value.popperType
+                              }
+                            />
+                            {value.name}
+                          </label>
+                        );
+                      })}
+                    </div>
+                    <div className="style-selector">
+                      Placement
+                      {positionToName.map(value => {
+                        return (
+                          <label key={value.position}>
+                            <input
+                              type="radio"
+                              name="position"
+                              onChange={this.getOnPositionClick(value.position)}
+                              defaultChecked={
+                                this.state.position === value.position
+                              }
+                            />
+                            {value.name}
+                          </label>
+                        );
+                      })}
+                    </div>
+                  </div>
+                  <div className="examples">
+                    <p>Real world examples:</p>
+                    <ul>
+                      <li>
+                        Hover + Hover -{" "}
+                        <a
+                          href="https://material-ui-next.com/demos/tooltips/"
+                          target="_blank"
+                        >
+                          Material UI's Tooltip
+                        </a>
+                      </li>
+                      <li>
+                        Click + Click -{" "}
+                        <a
+                          href="https://material-ui-next.com/demos/menus/"
+                          target="_blank"
+                        >
+                          Material UI's Menus
+                        </a>
+                      </li>
+                      <li>
+                        Click + Blur -{" "}
+                        <a
+                          href="https://jqueryui.com/selectmenu/"
+                          target="_blank"
+                        >
+                          jQuery UI's Select menu
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="popover-example">
+                  <div className="example">
+                    <Popover
+                      wrapperElementType={"span"}
+                      wrapperElementProps={{ className: "react-15" }}
+                      popperContent={
+                        <PopoverContent prefixText={this.state.popperType} />
+                      }
+                      popperOptions={{
+                        placement: this.state.position
+                      }}
+                      popperType={this.state.popperType}
+                      targetContent={"Target type - " + this.state.targetType}
+                      targetType={this.state.targetType}
+                    />
+                  </div>
+                </div>
+              </div>
+            )
+          }
+        ]}
+      />
+    );
+  }
 
-    getOnTargetTypeClick(targetType: TargetType) {
-        return () => {
-            this.setState({
-                targetType: targetType,
-            });
-        };
+  getOnTargetTypeClick(targetType: TargetType) {
+    return () => {
+      this.setState({
+        targetType: targetType
+      });
     };
+  }
 
-    getOnPopperTypeClick(popperType: PopperType) {
-        return () => {
-            this.setState({
-                popperType: popperType
-            });
-        };
+  getOnPopperTypeClick(popperType: PopperType) {
+    return () => {
+      this.setState({
+        popperType: popperType
+      });
     };
+  }
 
-    getOnPositionClick(position: PopperPositionOptions) {
-        return () => {
-            this.setState({
-                position: position
-            });
-        };
+  getOnPositionClick(position: PopperPositionOptions) {
+    return () => {
+      this.setState({
+        position: position
+      });
     };
+  }
 }
 
 interface PopoverContentProps {
-    prefixText: string
+  prefixText: string;
 }
 
 interface PopoverContentState {
-    popperMoreCount: number
+  popperMoreCount: number;
 }
 
-class PopoverContent extends React.Component<PopoverContentProps, PopoverContentState> {
-    constructor(props: PopoverContentProps) {
-        super(props);
+class PopoverContent extends React.Component<
+  PopoverContentProps,
+  PopoverContentState
+> {
+  constructor(props: PopoverContentProps) {
+    super(props);
 
-        this.state = {
-            popperMoreCount: 0
-        }
-    }
+    this.state = {
+      popperMoreCount: 0
+    };
+  }
 
-    render() {
-        return <div className="popper-content">
-            <div className="text">Popper type - {this.props.prefixText}</div>
-            <ul>{new Array(this.state.popperMoreCount).fill(0).map((value, index) => <li key={index}>{`Item #${index}`}</li>)}
-            </ul>
-            <div className="buttons">
-                <button onClick={this.onRemoveItem}>Remove item</button>
-                <button onClick={this.onAddItem}>Add item</button>
-            </div>
-        </div>;
-    }
+  render() {
+    return (
+      <div className="popper-content">
+        <div className="text">Popper type - {this.props.prefixText}</div>
+        <ul>
+          {new Array(this.state.popperMoreCount).fill(0).map((value, index) => (
+            <li key={index}>{`Item #${index}`}</li>
+          ))}
+        </ul>
+        <div className="buttons">
+          <button onClick={this.onRemoveItem}>Remove item</button>
+          <button onClick={this.onAddItem}>Add item</button>
+        </div>
+      </div>
+    );
+  }
 
-    onRemoveItem = () => {
-        if (this.state.popperMoreCount != 0) {
-            this.setState({
-                popperMoreCount: this.state.popperMoreCount - 1
-            });
-        }
+  onRemoveItem = () => {
+    if (this.state.popperMoreCount != 0) {
+      this.setState({
+        popperMoreCount: this.state.popperMoreCount - 1
+      });
     }
+  };
 
-    onAddItem = () => {
-        this.setState({
-            popperMoreCount: this.state.popperMoreCount + 1
-        });
-    }
+  onAddItem = () => {
+    this.setState({
+      popperMoreCount: this.state.popperMoreCount + 1
+    });
+  };
 }
